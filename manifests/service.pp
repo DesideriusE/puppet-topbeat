4c8c2e03ecf7a3a53d997ea2a22d4317e61169f3
Paul Kranenburg <kranenburg@ict.eur.nl>
2016-11-29 18:29:35 +0100

    Derive service 'ensure' parameter from base class

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,17 @@
 #
 class topbeat::service (
-  $ensure             = $topbeat::ensure
-){
-  
+  $ensure = $::topbeat::ensure
+) {
+ 
+  $_ensure = $::filebeat::ensure ? {
+    present => 'running',
+    absent  => 'stopped',
+    default => 'running'
+  }
+
   service { 'topbeat':
-    ensure => 'running',
+    ensure => $_ensure,
     enable => true
   }
-  
+
 }
